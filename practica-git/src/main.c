95c5956195188c119a5038b467ecfc096ec302c4
Lucas Rack <lrack@fi.uba.ar>
2020-10-08 20:58:19 -0300

    primer aproach al problema

--- practica-git/src/main.c
+++ practica-git/src/main.c
@@ -1,18 +1,42 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 #define MAX_BUFFER 500
 
-int main(int argc, char const *argv[]){
-	char buffer[MAX_BUFFER];
-	bool reading = true;
-	
-	while(reading){
-		fgets(buffer,MAX_BUFFER,stdin);
-		if(strcmp()){
-			
+int check_number(char * s){
+	int len = strlen(s);
+	for(int i = 0; i < len; ++i){
+		if(isalpha(s[i])){
+			return 1;
 		}
 	}
-	
+	return 0;
+}
 
+int main(int argc, char const *argv[]){
+	char buffer[MAX_BUFFER];
+	double sum = 0;
+	double mul = 1;
+	double aux;
+
+	while(1){
+		fgets(buffer,MAX_BUFFER,stdin);
+		if(strncmp(buffer,"sum",3) == 0){
+			printf("Resultado: %f\n", sum);
+			return 0;
+		} else if (strncmp(buffer,"mul",3) == 0){
+			printf("Resultado: %f\n", mul);
+			return 0;
+		}
+		aux = strtod (buffer, NULL);
+		if(check_number(buffer)){
+			fprintf(stderr, "ERROR\n");
+			return 1;
+		}
+		sum += aux;
+		mul *=aux;
+	}
+	return 1;
 }
